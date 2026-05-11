a9701326fc6c75d233f7753d336fccb797215d7c
Camy Di Mario <166224855+Camydimario@users.noreply.github.com>
2026-05-11 15:33:59 -0300

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -8,12 +8,16 @@ public class Ticket {
 	double importe = 0;
 	int cantidad=0;
     double precioUnitario=0;
-	int item=0;
-	int subtotal= 0;
+	int productoTotales=0;
+	double subtotal= 0;
 	double descuento= 0;
 double precioFinal=0;
 	boolean abierto = true;
-    public Ticket() {
+    
+	
+	
+	
+	public Ticket() {
     	int importe = 0;
     	this.importe = importe;
     	
@@ -27,9 +31,10 @@ double precioFinal=0;
     public void agregarItem(int cantidad, double precioUnitario) {
     	if(cantidad > 0 && precioUnitario > 0) {
     		this.importe = cantidad * precioUnitario;
+    	    subtotal = cantidad * precioUnitario;
+    		productoTotales += cantidad;
     		importe++;
-    		subtotal++;
-    		
+    		System.out.println("Se inicia la compra de " + cantidad + " productos al precio de $" + precioUnitario);
     		}
     	}
 
@@ -40,6 +45,8 @@ double precioFinal=0;
     public void aplicarDescuento(double porcentaje) { 
     	this.descuento = importe * (porcentaje /100);
     	precioFinal = importe - descuento;
+    	System.out.println("Se aplica el descuento del " + porcentaje + " %." + " El precio es de $" + descuento  + ". Queda un total de $" + precioFinal);
+    	subtotal++;
     }
 
     /**
@@ -48,45 +55,58 @@ double precioFinal=0;
      */
     public double calcularSubtotal() {
     	return subtotal;
+    	//Es la suma del precio por producto nomas sin descuentos aplicados
     }
 	
     /**
      * post: cierra el Ticket y devuelve el importe total.
      */
     public double calcularTotal() {
-    	return importe + subtotal;
+    	return precioFinal + subtotal;
+    	//Es la suma del precio por producto coon descuentos aplicados
     }
 
     /**
      * post: devuelve la cantidad total de productos.
      */
     public int contarProductos() {
-    	return cantidad;
+    	return productoTotales;
     }
     
 public static void main (String []args) {
 	Ticket tickesito = new Ticket();
-	
-	tickesito.agregarItem(3, 40.69);
+	System.out.println("-----Compra N°1-----" );
+	tickesito.agregarItem(3, 40.50);
 
 	tickesito.aplicarDescuento(25);
 	
-	tickesito.calcularTotal();
+	System.out.println("El Subtotal es de : $" + tickesito.calcularSubtotal());
+	System.out.println("El Total es de: $" + tickesito.calcularTotal());
+	System.out.println("La Cantitad de Productos total es : " + tickesito.contarProductos());
 	
-	tickesito.calcularTotal();
+	System.out.println("-----Compra N°2-----" );
+	tickesito.agregarItem(5, 70.00);
+
+	tickesito.aplicarDescuento(15);
 	
-	tickesito.contarProductos();
+	System.out.println("El Subtotal es de : $" + tickesito.calcularSubtotal());
+	System.out.println("El Total es de: $" + tickesito.calcularTotal());
+	System.out.println("La Cantitad de Productos total es : " + tickesito.contarProductos());
 	
 	
+	System.out.println("-----Compra N°3-----" );
+	tickesito.agregarItem(1, 31.0);
+
+	tickesito.aplicarDescuento(30);
 	
+	System.out.println("El Subtotal es de : $" + tickesito.calcularSubtotal());
+	System.out.println("El Total es de: $" + tickesito.calcularTotal());
+	System.out.println("La Cantitad de Productos total es : " + tickesito.contarProductos());
 	
-	
-	
-	
+
 }
 
-	
-	
-	
-	
 }
+	
+	
+
