20269eb6a5d235bc601e6a2b594eff996d216d8b
Camy Di Mario <166224855+Camydimario@users.noreply.github.com>
2026-06-02 17:46:10 -0300

    Create monedas.java

---
+++ monedas.java
@@ -0,0 +1,33 @@
+package Numismatico;
+
+public class monedas {
+private String pais;
+private int anio;
+private int valor;
+	
+	public monedas (String pais, int anio , int valor) {
+		this.pais = pais;
+		this.anio=anio;
+		this.valor=valor;
+	}
+	
+	
+	public String getPais() {
+		return pais;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public int getAnio() {
+		return anio;
+	}
+	
+	@Override
+	public String toString() {
+	    return "Moneda: " + pais + " (" + anio + ") - Valor: $" + valor;
+	}
+	
+	
+	}
